b91cd2189d990f34ea72e0f09f5c9965dd9f3a8e
daniel0128 <survey600@qq.com>
2016-03-05 15:27:41 -0800

    parallel works

---
+++ Parallel.js
@@ -0,0 +1,118 @@
+var margin = {left:10,right:10,top:30,bottom:10},
+    w = 1236 - margin.left - margin.right,
+    h = 700 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal().rangePoints([0, w], 1),
+    y = {},
+    dragging = {};
+
+var line = d3.svg.line(),
+    axis = d3.svg.axis().orient("left"),
+    background,
+    foreground;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", w + margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function render(error,data){
+    if(error) throw error;
+    x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
+        return y[d] = d3.scale.linear()
+            .domain(d3.extent(data, function(p) { return +p[d]; }))
+            .range([h, 0]);
+    }));
+
+    background = svg.append("g")
+        .attr("class", "background")
+        .selectAll("path")
+        .data(data)
+        .enter().append("path")
+        .attr("d", path);
+
+    foreground = svg.append("g")
+        .attr("class", "foreground")
+        .selectAll("path")
+        .data(data)
+        .enter().append("path")
+        .attr("d", path);
+
+    var g = svg.selectAll(".dimension")
+        .data(dimensions)
+        .enter().append("g")
+        .attr("class", "dimension")
+        .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+        .call(d3.behavior.drag()
+            .on("dragstart", function(d) {
+                dragging[d] = this.__origin__ = x(d);
+                background.attr("visibility", "hidden");
+            })
+            .on("drag", function(d) {
+                dragging[d] = Math.min(w, Math.max(0, this.__origin__ += d3.event.dx));
+                foreground.attr("d", path);
+                dimensions.sort(function(a, b) { return position(a) - position(b); });
+                x.domain(dimensions);
+                g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
+            })
+            .on("dragend", function(d) {
+                delete this.__origin__;
+                delete dragging[d];
+                transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                transition(foreground)
+                    .attr("d", path);
+                background
+                    .attr("d", path)
+                    .transition()
+                    .delay(500)
+                    .duration(0)
+                    .attr("visibility", null);
+            }));
+
+    g.append("g")
+        .attr("class", "axis")
+        .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+        .append("text")
+        .attr("text-anchor", "middle")
+        .attr("y", -9)
+        .text(String);
+
+    // Add and store a brush for each axis.
+    g.append("g")
+        .attr("class", "brush")
+        .each(function(d) { d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush)); })
+        .selectAll("rect")
+        .attr("x", -8)
+        .attr("width", 16);
+}
+function position(d) {
+    var v = dragging[d];
+    return v == null ? x(d) : v;
+}
+
+function transition(g) {
+    return g.transition().duration(500);
+}
+
+// Returns the path for a given data point.
+function path(d) {
+    return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+}
+
+// When brushing, don’t trigger axis dragging.
+function brushstart() {
+    d3.event.sourceEvent.stopPropagation();
+}
+
+// Handles a brush event, toggling the display of foreground lines.
+function brush() {
+    var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
+        extents = actives.map(function(p) { return y[p].brush.extent(); });
+    foreground.style("display", function(d) {
+        return actives.every(function(p, i) {
+            return extents[i][0] <= d[p] && d[p] <= extents[i][1];
+        }) ? null : "none";
+    });
+}
+//d3.tsv("cadata.tsv",render);
